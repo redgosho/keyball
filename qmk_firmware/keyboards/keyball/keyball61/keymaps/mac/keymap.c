406bcba9da3c65da321060dcee9f348ec79c11c0
redgosho <redgosho@gmail.com>
2023-04-08 13:56:00 +0900

    macの設定左したキー配置を変更

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/mac/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/mac/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U       , KC_I     , KC_O     , KC_P     , KC_EQUAL ,
     MO(1)    , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J       , KC_K     , KC_L     , KC_SCLN  , KC_QUOTE ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LNG2  ,              KC_LNG1, KC_N     , LT(3,KC_M) , KC_COMM  , KC_DOT   , KC_SLSH  , MO(3)  ,
-    KC_LCTL  , KC_LGUI  , KC_LALT  , KC_SPC,LT(3,KC_SPC),LT(4,KC_LNG2),LT(2,KC_LNG1),         KC_BSPC ,LT(1,KC_ENT) , _______ , _______  , _______  , _______  , _______
+    KC_LCTL  , KC_LCTL  , KC_LALT  , KC_LGUI,LT(3,KC_SPC),LT(4,KC_LNG2),LT(2,KC_LNG1),         KC_BSPC ,LT(1,KC_ENT) , _______ , _______  , _______  , _______  , _______
   ),
 
   [1] = LAYOUT_universal(
